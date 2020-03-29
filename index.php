ffdaa013336ef966e87b1c922014777a5f93e569
nakano0518 <m15sc018.prog@gmail.com>
2020-03-29 12:04:50 +0000

    20200329 code refactoring

--- index.php
+++ index.php
@@ -3,21 +3,16 @@
 	session_regenerate_id(true);
 
 	require_once './lib/util.php';	
-
+	
+	require_once './vendor/autoload.php';
+	$dotenv = Dotenv\Dotenv::createImmutable('../env/');
+	$dotenv->load();
+	
 	if(isset($_SESSION['userId'])) {
 		$userId = $_SESSION['userId'];
 	}	
 
-	$user = 'nakano';
-	$dbpassword = '3114yashi';
-	$dbName = 'BookReview';
-	$host = 'techbookreview.ccbw4hq0h1r9.ap-northeast-1.rds.amazonaws.com';
-    	$dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
 	try{
-		$pdo = new PDO($dsn, $user, $dbpassword);
-		$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);//プリペアドステートメントのエミュレーション無効
-        	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//例外がスローされる設定にする
-		
 		/*-------------------------------------------------------------------------------------------------------------
 		キーワード検索(SQL文のWHERE句を作成)
 		-------------------------------------------------------------------------------------------------------------*/
@@ -123,10 +118,10 @@
 		//1ページあたり何冊表示するか
 		define('max_item', 12);
 		//必要なページ数を求める
-		$count = $pdo->prepare('SELECT COUNT(*) AS count FROM t_book'.$where);
-		$count->execute();
-
-		$total_count = $count->fetch(PDO::FETCH_ASSOC);
+		$db = new DbConnect(getenv("DB_USERNAME"), getenv("DB_PASSWORD"), getenv("DB_DATABASE"), getenv("DB_HOST"));
+		$db->createPdo();
+		$sql = 'SELECT COUNT(*) AS count FROM t_book'.$where;
+		$total_count = $db->selectfetch($sql);	
 		$pages = ceil($total_count['count'] / max_item);
 
 		//現在いるページ番号の取得
@@ -139,196 +134,202 @@
 
 		//表示する本のコンテンツを取得するSQL
 		$sql = 'SELECT t_book.bookTitle, t_book.imageUrl, t_book.no, t_book.created_at'.$counter.' FROM t_book'.$join.$where.$orderBy.' LIMIT :start, :max';
-		$select = $pdo->prepare($sql);
+		
 		if($now ==1){
    			//1ページ目の処理
-    			$select->bindValue(':start', $now-1, PDO::PARAM_INT);
-    			$select->bindValue(':max', max_item, PDO::PARAM_INT);
+			$data = $db->selectfetchAll($sql,[
+    				[':start', $now-1, PDO::PARAM_INT],
+    				[':max', max_item, PDO::PARAM_INT]
+			]);
 		}else{
-    			//1ページ目以外の処理
-    			$select->bindValue(':start', ($now-1) * max_item, PDO::PARAM_INT);
-    			$select->bindValue(':max', max_item, PDO::PARAM_INT);
+    			//1ページ目以外の処理	
+			$data = $db->selectfetchAll($sql,[
+    				[':start', ($now-1) * max_item, PDO::PARAM_INT],
+    				[':max', max_item, PDO::PARAM_INT]
+			]);
 		}
-		//実行し結果を取り出す
-		$select->execute();
-		$data = $select->fetchAll(PDO::FETCH_ASSOC);
 	}catch(Exception $e) {
+		echo '<span class="error">エラーがありました。</span><br>';
+        	echo $e->getMessage().'<br>';
 	}
 ?>
 
 <?php
 	/*----------------------------------------------------------------------
-    	ドロワーメニューに表示するユーザー名の取得 
+    	モバイル用ドロワーメニューに表示するユーザー名の取得 
 	----------------------------------------------------------------------*/
 	if(isset($_SESSION['userId'])||!empty($_SESSION['userId'])){
     		try{
-        		$pdo = new PDO($dsn, $user, $dbpassword);
-			$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);//プリペアドステートメントのエミュレーション無効
-        		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//例外がスローされる設定にする
+			$db = new DbConnect(getenv("DB_USERNAME"), getenv("DB_PASSWORD"), getenv("DB_DATABASE"), getenv("DB_HOST"));
+    			$db->createPdo();
         		$sql = "SELECT name FROM t_users WHERE userId = :userId";
-        		$select = $pdo->prepare($sql);
-        		$select->bindValue(':userId', $userId, PDO::PARAM_STR);
-        		$select->execute();
-        		$name = $select->fetch(PDO::FETCH_ASSOC);
+        		$name = $db->selectfetch($sql, [[":userId", $userId, PDO::PARAM_STR]]);
    		}catch(Exception $e){
-        		echo $e->getMessage();
+			echo '<span class="error">エラーがありました。</span><br>';
+        		echo $e->getMessage().'<br>';
     		}
 	}
 	
 ?>
+
+
+
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">    
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono|Sawarabi+Gothic&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./css/index.css">
-    <title>TechBookLabo | エンジニアのための書籍レビューサイト</title>
-</head>
-<body>
-    <!--レイヤー(sortモーダルのための背景透過レイヤー)-->
-    <!-- ドロワーメニュー -->
-        <div class="drawer">
-            <div class="drawerMenu">
-                <?php if(isset($userId) && !empty($userId)): ?>
-                    <div class="signInMenu">
-                        <p>ようこそ「<?php echo isset($name['name']) && !empty($name['name']) ? $name['name'] : "名称未設定" ; ?>」さん</p>
-                        <ul>
-			    <li><a href="./index.php">ホーム</a></li>
-                            <li><a href="./views/mypage.php">マイページ</a></li>
-                            <li><a href="./views/signOut.php">ログアウト</a></li>
-                        </ul>
-                    </div>
-                <?php else: ?>
-                    <div class="signOutMenu">
-                        <p>ログイン or 新規登録</p>
-                        <ul>
-			    <li><a href="./index.php">ホーム</a></li>
-                            <li><a href="./views/signUp.php">新規登録</a></li>
-                            <li><a href="./views/signIn.php">ログイン</a></li>
-                        </ul>
-                    </div>
-                <?php endif; ?>
-            </div>
-            <div class="cancelDrawerLayer">
-            </div>
-        </div>
-    <header>
-        <section class="header">
-        <img src="./images/booklogo.svg">
-        <div class="headercenter">
-            <a href="./index.php">
-                <span class="title">TechBookLabo</span>
-            </a>
-        </div>
-        <div class="headerright">
-            <i class="fas fa-bars"></i>
-            <div class="menus">
-                <ul>
-                    <li><a href="./views/signUp.php">新規登録</a></li>
-                    <li class="<?php echo isset($userId)||!empty($userId) ? 'invisible': ''; ?>">
-                        <a href="./views/signIn.php" class="<?php echo $userId ? 'invisible': ''; ?>">ログイン</a>
-                    </li>
-                    <li class="<?php echo isset($userId)||!empty($userId) ? 'visible': 'invisible'; ?>">
-                        <a href="./views/mypage.php" class="<?php echo isset($userId)||!empty($userId) ? 'visible': 'invisible'; ?>">マイページ</a>
-                    </li>
-                    <li class="<?php echo isset($userId)||!empty($userId) ? 'visible': 'invisible'; ?>">
-                        <a href="./views/signOut.php" class="<?php echo isset($userId)||!empty($userId) ? 'visible': 'invisible'; ?>">ログアウト</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        </section>
-    </header>
-    <main>
-        <section class="controlSection">
-            <div class="component">
-                <form action='<?php echo "./index.php?".$querySelectInput.$querySort."&page_id=".$n ; ?>' method="get">
-                    <div id="searchForm">
-                        <input type="text" name="searchInput" placeholder="書籍名やジャンルを入力" value="<?php echo empty($_GET['searchInput'])? '': es($_GET['searchInput']); ?>">
-                        <button type="submit"><i class="fas fa-search"></i></button>
-                    </div>
-                    <button class="sort">
-                        <i class="fas fa-sort-down"></i>
-                        <span class="sortDescription">並べ替え</span>
-                    </button>
-                    	<div class="sortForm tag">
-                      		<button type="submit" name="sort" value="1">価格の安い順</button>
-                      		<button type="submit" name="sort" value="2">価格の高い順</button>
-                      		<button type="submit" name="sort" value="3">ページ数の少ない順</button>
-                      		<button type="submit" name="sort" value="4">ページ数の多い順</button>
-                      		<button type="submit" name="sort" value="5">お気に入り数の少ない順</button>
-                      		<button type="submit" name="sort" value="6">お気に入り数のの多い順</button>
-                      		<button type="submit" name="sort" value="7">レビュー数の少ない順</button>
-                      		<button type="submit" name="sort" value="8">レビュー数の多い順</button>
-		    </div>
+	<head>
+    		<meta charset="UTF-8">
+    		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">    
+    		<link href="https://fonts.googleapis.com/css?family=Roboto+Mono|Sawarabi+Gothic&display=swap" rel="stylesheet">
+    		<link rel="stylesheet" href="./css/index.css">
+    		<title>TechBookLabo | エンジニアのための書籍レビューサイト</title>
+	</head>
+	
+	<body>
+    		<!-- モバイル用ドロワーメニューここから -->
+        	<div class="drawer">
+            		<div class="drawerMenu">
+                		<?php if(isset($userId) && !empty($userId)): ?>
+                    			<div class="signInMenu">
+                        			<p>ようこそ「<?php echo isset($name['name']) && !empty($name['name']) ? $name['name'] : "名称未設定" ; ?>」さん</p>
+                        			<ul>
+			    				<li><a href="./index.php">ホーム</a></li>
+                            				<li><a href="./views/mypage.php">マイページ</a></li>
+                            				<li><a href="./views/signOut.php">ログアウト</a></li>
+                        			</ul>
+                    			</div>
+                		<?php else: ?>
+                    			<div class="signOutMenu">
+                        			<p>ログイン or 新規登録</p>
+                        			<ul>
+			    				<li><a href="./index.php">ホーム</a></li>
+                           		 		<li><a href="./views/signUp.php">新規登録</a></li>
+                            				<li><a href="./views/signIn.php">ログイン</a></li>
+                        			</ul>
+                    			</div>
+                		<?php endif; ?>
+            		</div>
+            		<div class="cancelDrawerLayer">
+			</div>
+        	</div>
+    		<!-- モバイル用ドロワーメニューここまで -->
+    		
+		<header>
+        		<section class="header">
+        			<img src="./images/booklogo.svg">
+        			<div class="headercenter">
+            				<a href="./index.php">
+                				<span class="title">TechBookLabo</span>
+            				</a>
+        			</div>
+        			<div class="headerright">
+            				<i class="fas fa-bars"></i>
+            				<div class="menus">
+                				<ul>
+                    					<li><a href="./views/signUp.php">新規登録</a></li>
+                    					<li class="<?php echo isset($userId)||!empty($userId) ? 'invisible': ''; ?>">
+                        					<a href="./views/signIn.php" class="<?php echo $userId ? 'invisible': ''; ?>">ログイン</a>
+                    					</li>
+                    					<li class="<?php echo isset($userId)||!empty($userId) ? 'visible': 'invisible'; ?>">
+                        					<a href="./views/mypage.php" class="<?php echo isset($userId)||!empty($userId) ? 'visible': 'invisible'; ?>">マイページ</a>
+                    					</li>
+                    					<li class="<?php echo isset($userId)||!empty($userId) ? 'visible': 'invisible'; ?>">
+                        					<a href="./views/signOut.php" class="<?php echo isset($userId)||!empty($userId) ? 'visible': 'invisible'; ?>">ログアウト</a>
+                    					</li>
+                				</ul>
+            				</div>
+        			</div>
+        		</section>
+    		</header>
+    
+		<main>
+        		<section class="controlSection">
+            			<div class="component">
+                			<form action='<?php echo "./index.php?".$querySelectInput.$querySort."&page_id=".$n ; ?>' method="get">
+                    				<div id="searchForm">
+                        				<input type="text" name="searchInput" placeholder="書籍名やジャンルを入力" value="<?php echo empty($_GET['searchInput'])? '': es($_GET['searchInput']); ?>">
+                        				<button type="submit"><i class="fas fa-search"></i></button>
+                    				</div>
+                    				<button class="sort">
+                        				<i class="fas fa-sort-down"></i>
+                        				<span class="sortDescription">並べ替え</span>
+                    				</button>
+                    				<div class="sortForm tag">
+                      					<button type="submit" name="sort" value="1">価格の安い順</button>
+                      					<button type="submit" name="sort" value="2">価格の高い順</button>
+                      					<button type="submit" name="sort" value="3">ページ数の少ない順</button>
+                      					<button type="submit" name="sort" value="4">ページ数の多い順</button>
+                      					<button type="submit" name="sort" value="5">お気に入り数の少ない順</button>
+                      					<button type="submit" name="sort" value="6">お気に入り数のの多い順</button>
+                      					<button type="submit" name="sort" value="7">レビュー数の少ない順</button>
+                      					<button type="submit" name="sort" value="8">レビュー数の多い順</button>
+		    				</div>
 
-                </form>
-                <button class="bookInsert" onclick="location.href='./views/bookInsert.php'">
-                    <i class="fas fa-plus"></i>
-                    <span class="bookInsertDescription">書籍の登録</span>
-                </button>
-            </div>
-        </section>
-    <section class="flexContainer">
-    <?php
-    foreach($data as $row) {
-        $no = $row['no'];
-    ?>
-　　 <div class="book">
-       <a href="./views/bookDetail.php?bookNo=<?= $no; ?>">
-        <img src="<?php echo $row['imageUrl']; ?>" width="150" height="180"><br>
-       <p class="capture"><?= $row['bookTitle']; ?></p>
-       </a>
-     </div>
-    <?php
-    }
-    ?>
-    <div class="book"></div><div class="book"></div><div class="book"></div><div class="book"></div><div class="book"></div>
-    </section>
-    <section class="page">
-        <?php
-	if($now == 1){	
-                echo "<span style='padding:5px;font-size:18px;'>{$now}</span>";
-	}else{
-                echo "<a href='./index.php?{$querySelectInput}{$querySort}page_id=1'
-	        style='padding:5px 10px;font-size:18px;border:1px solid #000;text-decoration:none;color:#000;'>1</a>";
-	}
-	if($now>=4){
-		echo "・・・";
-	}
-        for($n=2; $n<=$pages-1;$n++){
-	    if($n==$now){
-                echo "<span style='padding:5px;font-size:18px;'>{$now}</span>";
-            }else if($n == $now-1 || $n == $now+1){
-                echo "<a href='./index.php?{$querySelectInput}{$querySort}page_id=$n'
-                style='padding:5px 10px;font-size:18px;border:1px solid #000;text-decoration:none;color:#000;'>{$n}</a>";
-            }else{
-		continue;
-	    }
-        }
-	if($now < $pages-2){
-		echo "・・・";
-	}
-	
-	if($pages == 1){
-	
-	}else if($now == $pages){
-       		 echo "<span style='padding:5px;font-size:18px;'>{$pages}</span>";
-	}else{
-        	echo "<a href='./index.php?{$querySelectInput}{$querySort}page_id=$pages'
-	        style='padding:5px 10px;font-size:18px;border:1px solid #000;text-decoration:none;color:#000;'>{$pages}</a>";
-	}    
-        ?>
-    </section>
-    </main>
-    <footer>
-        <p><small>&copy; 2019 TAICHI NAKANO</small></p>
-    </footer>
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-    <script src="./js/index.js"></script>
-    <script src="./js/layout.js"></script>
-</body>
+                			</form>	
+                			<button class="bookInsert" onclick="location.href='./views/bookInsert.php'">
+                    				<i class="fas fa-plus"></i>
+                    				<span class="bookInsertDescription">書籍の登録</span>
+                			</button>
+            			</div>
+        		</section>
+    			<section class="flexContainer">
+    				<?php
+    					foreach($data as $row) {
+        					$no = $row['no'];
+    				?>
+　　 					<div class="book">
+       						<a href="./views/bookDetail.php?bookNo=<?= $no; ?>">
+        						<img src="<?php echo $row['imageUrl']; ?>" width="150" height="180"><br>
+       							<p class="capture"><?= $row['bookTitle']; ?></p>
+       						</a>
+     					</div>
+    				<?php
+    					}
+    				?>
+    				<div class="book"></div><div class="book"></div><div class="book"></div><div class="book"></div><div class="book"></div>
+    			</section>
+    			<!-- ページネーションここから -->
+			<section class="page">
+        			<?php
+					if($now == 1){	
+                				echo "<span style='padding:5px;font-size:18px;'>{$now}</span>";
+					}else{
+                				echo "<a href='./index.php?{$querySelectInput}{$querySort}page_id=1' style='padding:5px 10px;font-size:18px;border:1px solid #000;text-decoration:none;color:#000;'>1</a>";
+					}
+					if($now>=4){
+						echo "・・・";
+					}
+        				for($n=2; $n<=$pages-1;$n++){
+	    					if($n==$now){
+                					echo "<span style='padding:5px;font-size:18px;'>{$now}</span>";
+            					}else if($n == $now-1 || $n == $now+1){
+                					echo "<a href='./index.php?{$querySelectInput}{$querySort}page_id=$n' style='padding:5px 10px;font-size:18px;border:1px solid #000;text-decoration:none;color:#000;'>{$n}</a>";
+            					}else{
+							continue;
+	    					}
+        				}
+					if($now < $pages-2){
+						echo "・・・";
+					}
+					if($pages == 1){
+						//何もしない
+					}else if($now == $pages){
+       		 				echo "<span style='padding:5px;font-size:18px;'>{$pages}</span>";
+					}else{
+        					echo "<a href='./index.php?{$querySelectInput}{$querySort}page_id=$pages' style='padding:5px 10px;font-size:18px;border:1px solid #000;text-decoration:none;color:#000;'>{$pages}</a>";
+					}    
+        			?>
+    			</section>
+    			<!-- ページネーションここまで -->
+    		</main>
+    
+		<footer>
+        		<p><small>&copy; 2019 TAICHI NAKANO</small></p>
+    		</footer>
+    
+		<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+    		<script src="./js/index.js"></script>
+    		<script src="./js/layout.js"></script>
+	</body>
 </html>
