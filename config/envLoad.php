69336ec10d0451b9cdf33ea0a530fef5eb933518
nakano0518 <m15sc018.prog@gmail.com>
2020-12-22 14:13:58 +0000

    fix

--- config/envLoad.php
+++ config/envLoad.php
@@ -1,6 +1,9 @@
 <?php
     require_once realpath(__DIR__.'/../').'/vendor/autoload.php';
-    // $dotenv = Dotenv\Dotenv::createImmutable(realpath(__DIR__.'/../').'/');
-    // $dotenv->load();
+    if(__DIR__ == '/home/ec2-user/environment/config'){ //ローカルやAWSデプロイの際に必要であったが、Herokuでは不要(GUIで設定)
+        $dotenv = Dotenv\Dotenv::createImmutable(realpath(__DIR__.'/../').'/');
+        $dotenv->load();  
+    }
+    
     
    
